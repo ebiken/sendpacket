027565dbc09ea589e1486b1ac15e82202cde21c0
Kentaro Ebisawa <ebiken.g@gmail.com>
2025-02-09 17:59:01 +0900

    Add PFC gen using scapy.contrib.mac_control

---
+++ pfc_gen.py
@@ -0,0 +1,21 @@
+from scapy.all import *
+from scapy.contrib.mac_control import *
+import sys
+
+frm = Ether(src='00:01:01:01:01:01', dst=MACControl.DEFAULT_DST_MAC)/ \
+      MACControlClassBasedFlowControl(c3_enabled=1, c3_pause_time=0xffff)
+
+if len(sys.argv) < 2:
+  print("Usage: python3 pfc_gen.py <interface> [number of packets to send]")
+  sys.exit(1)
+else:
+  interface = sys.argv[1]
+
+if len(sys.argv) < 3:
+  num = 1
+else:
+  num = sys.argv[2]
+
+print(f"sending {num} packets to {interface}")
+
+sendp(frm,iface=interface, count=int(num))
